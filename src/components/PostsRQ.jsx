d28a8a45df5c54de42b8387c777b1b1885cec1dd
Shubham Goyal <goyalshubham2521@gmail.com>
2024-09-06 15:18:21 +0530

    Data fetching using React Query

--- src/components/PostsRQ.jsx
+++ src/components/PostsRQ.jsx
@@ -1,5 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import "../App.css";
+
 const PostsRQ = () => {
-  return <div className="container">PostsRQ</div>;
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"],
+    //a callback fnc that always returns a promise
+    queryFn: () => {
+      return axios.get("http://localhost:4000/posts");
+    },
+  });
+
+  //   console.log(data);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div className="post-list">
+      {data?.data.map((post) => (
+        <div key={post.id} className="post-item">
+          <h3 className="post-title"> {post.title}</h3>
+          <p className="post-body">{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostsRQ;
