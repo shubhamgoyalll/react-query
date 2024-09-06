9875fdaabefc4bdeb44732a5b93287c2ad3d41f4
Shubham Goyal <goyalshubham2521@gmail.com>
2024-09-07 02:32:55 +0530

    Stale time and polling using RTQ

--- src/components/PostsRQ.jsx
+++ src/components/PostsRQ.jsx
@@ -3,15 +3,24 @@ import axios from "axios";
 import "../App.css";
 
 const PostsRQ = () => {
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isLoading, isError, error, isFetching } = useQuery({
+    //can add query params in this array
     queryKey: ["posts"],
     //a callback fnc that always returns a promise
     queryFn: () => {
       return axios.get("http://localhost:4000/posts");
     },
+    // for next 10 sec,the network call will not be made
+    // staleTime: 10000,
+
+    //polling : refetches the data every mentioned time (1 sec in this case)
+    refetchInterval: 1000,
+    //will reftech data now even if we change tabs
+    refetchIntervalInBackground: true,
   });
 
   //   console.log(data);
+  console.log(isLoading, isFetching);
 
   if (isLoading) {
     return <div>Loading...</div>;
